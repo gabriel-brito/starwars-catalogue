b8d7aab6b3dc371cc4a86682451d02f9af3d32a7
gabriel-brito <mfkrgabriel@gmail.com>
2017-09-13 00:27:27 -0300

    Trying to render Planet Residents

--- src/js/app.js
+++ src/js/app.js
@@ -2,7 +2,10 @@ const url = 'https://swapi.co/api/people/';
 const searchButton = document.querySelector("#searchButton");
 const searchResultsTable = document.querySelector("#itemlist");
 const resultsResults = document.querySelector("#searchResults");
+const kek = document.querySelector('#kappapride');
 let data = null;
+let planet = null;
+let planetResidents = null;
 
 const renderPersonsTable = (container, data) => {
 	container.innerHTML = data.map((item, counter) => 
@@ -19,7 +22,33 @@ const fetchPerson = (url) => {
 			const result = data.results;
 			renderPersonsTable(searchResultsTable, result);
 			renderPersonDetails(".search-result a", searchResultsTable);
+			renderResidents(planetResidents);
 		});
+}
+const fetchPlanetResidents = (homeworld) => {
+	fetch(homeworld)
+		.then(data => data.json())
+		.then(data=> {
+			planetResidents = data.residents;
+			planet = data.name;
+		});
+}
+
+const renderResidents = (planetResidents) => {
+	for(var key in planetResidents) {
+	    if(planetResidents.hasOwnProperty(key)) {
+	        console.log(planetResidents[key]);
+	        fetch(planetResidents[key])
+	        	.then(data => data.json())
+	        	.then(data=> {
+				//kek.innerHTML = 
+			//	`<tr class="search-result___content-item">
+			//		<td class="search-result"><a href="${planetResidents[key].url}">${planetResidents[key].name}</a></td>
+			//	</tr>`;
+	        	console.log(planetResidents[key].name);
+	        	});
+	    }
+	}
 }
 
 const renderPersonDetails = (keyMapElement, searchResultsContainer) => {
@@ -28,43 +57,31 @@ const renderPersonDetails = (keyMapElement, searchResultsContainer) => {
 		keysMap[key].onclick = function(e) {
 			e.preventDefault();
 			let path = this.href;
-			let planet = null;
-			let planetResidents = null;
 			let renderPlanetResidents = null;
 			fetch(path)
 				.then(data => data.json())
 				.then(data => {
-						let {name, birth_year, gender, homeworld} = data;
-						fetch(homeworld)
-							.then(data => data.json())
-							.then(data=> {
-								 planetResidents = data.residents;
-								 planet = data.name;
-								 for(var key in planetResidents) {
-								    if(planetResidents.hasOwnProperty(key)) {
-								        console.log(planetResidents[key]);
-								        fetch(planetResidents[key])
-								        	.then(data => data.json())
-								        	.then(data=> {
-								        		console.log(data.name);
-								        	});
-								    }
-								}
-								 searchResultsContainer.innerHTML = 
-			 						`<tr class="search-result___content-item">
-											<td class="search-result">
-												<p>Name: ${name}</p>
-												<p>Birth Year: ${birth_year}</p>
-												<p>Gender: ${gender}</p>
-												<p>Planet: ${planet}</p>
-												<p>${planet} Residents: </p>
-											</td>
-										</tr>`;
-							});
-					});
+					let {name, birth_year, gender, homeworld} = data;
+					fetchPlanetResidents(homeworld);
+					setTimeout(() => {
+						searchResultsContainer.innerHTML = 
+							`<tr class="search-result___content-item">
+								<td class="search-result">
+									<p>Name: ${name}</p>
+									<p>Birth Year: ${birth_year}</p>
+									<p>Gender: ${gender}</p>
+									<p>Planet: ${planet}</p>
+									<p>${planet} Residents: </p>
+									<table id="kappapride">
+									</table>
+								</td>
+							</tr>`;	
+					}, 2000);		
+				});
 		}
 	}
 }
+
 
 window.onload = fetchPerson(url);
 searchButton.addEventListener('click', (e) => {
@@ -76,7 +93,7 @@ searchButton.addEventListener('mouseover', ()=>{
 	audio.src = '../src/js/sounds/open.mp4';
 	audio.play();
 	setTimeout(() => {
-	  searchButton.innerText = `Don't ever become a sith!`;
+	  searchButton.innerText = `Welcome to the dark side`;
 	}, 700)
 });
 
