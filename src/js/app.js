bb341010f588a37368739aee70f6e43b6d556794
gabriel-brito <mfkrgabriel@gmail.com>
2017-09-18 00:27:18 -0300

    Changing names and improving code organization

--- src/js/app.js
+++ src/js/app.js
@@ -8,117 +8,110 @@ const searchResultsTable = document.querySelector("#itemlist");
 const resultsResults = document.querySelector("#searchResults");
 const searchInput = document.querySelector('#search-input');
 
-
 let data = null;
 let planet = null;
 let planetResidents = null;
 
 const pageLoaded = ()=>{
-	mainPage.style.display = 'block';
-	loader.style.display = 'none';
+  mainPage.style.display = 'block';
+  loader.style.display = 'none';
 }
 
 const cleaningPersonResult = (container)=>{
-	container.innerHTML = "";	
+  container.innerHTML = "";	
 }
 
 const searchFilter = ()=>{
-	let filter = searchInput.value.toUpperCase();
-	let row = Array.from(searchResultsTable.querySelectorAll('tr td'));
-	row.map(item => {
-		console.log(item);
-		item.textContent.toUpperCase().indexOf(filter) === -1 ? 
-		item.style.display = "none"
-		: item.style.display = ""
+  let filter = searchInput.value.toUpperCase();
+  let row = Array.from(searchResultsTable.querySelectorAll('tr td'));
+  row.map(item => {
+    console.log(item);
+	item.textContent.toUpperCase().indexOf(filter) === -1 ? 
+	item.style.display = "none"
+	: item.style.display = ""
 	});
 }
 
 const renderPersonsTable = (container, data) => {
-	container.innerHTML += data.map((item, counter) => 
-		`<tr class="search-result___content-item">
-			<td class="search-result">
-				<a class="search-result__person-name" href="${data[counter].url}">
-					${data[counter].name}
-				</a>
-			</td>
-		</tr>`).join('');
+  container.innerHTML += data.map((item, counter) => 
+	`<tr class="search-result___content-item">
+		<td class="search-result">
+			<a class="search-result__person-name" href="${data[counter].url}">
+				${data[counter].name}
+			</a>
+		</td>
+	</tr>`).join('');
 }
 
 const fetchPerson = (url) => {
-	fetch(url)
-		.then(data => data.json())
-		.then(data=> {
-			let result = data.results;
-			renderPersonsTable(searchResultsTable, result);
-			renderPersonDetails(".search-result a", searchResultsTable);
-				if (data.next)
-					fetchPerson(data.next);
+  fetch(url)
+    .then(data => data.json())
+	.then(data=> {
+	  let result = data.results;
+		renderPersonsTable(searchResultsTable, result);
+		renderPersonDetails(".search-result a", searchResultsTable);
+	      if (data.next)
+			fetchPerson(data.next);
 		});
 }
 const fetchPlanetResidents = (homeworld) => {
-	fetch(homeworld)
-		.then(data => data.json())
-		.then(data=> {
-			planetResidents = data.residents;
-			planet = data.name;
-		});
+  fetch(homeworld)
+	.then(data => data.json())
+	.then(data=> {
+	  planetResidents = data.residents;
+	  planet = data.name;
+	});
 }
 
 const renderResidents = (container, planetResidents) => {
-	for(var key in planetResidents) {
-	    if(planetResidents.hasOwnProperty(key)) {
-	        console.log(planetResidents[key]);
-	        let eachResident = planetResidents[key];
-	        fetch(eachResident)
-	        	.then(data => data.json())
-	        	.then(data=> {
-	        		console.log(data.name);
-	        		console.log(data.url);
-				container.innerHTML += 
-				`<tr class="search-result___content-item">
-					<td class="search-result"><a class="search-result__person-name" href="${data.url}">${data.name}</a></td>
-				</tr>`;
-				renderPersonDetails(".search-result a", searchResultsTable);
-	        	});
-	    }
-	}
-
+  for(var key in planetResidents) {
+	if(planetResidents.hasOwnProperty(key)) {
+	  let eachResident = planetResidents[key];
+	  fetch(eachResident)
+	   	.then(data => data.json())
+	   	.then(data=> {
+	      container.innerHTML += 
+		  `<tr class="search-result___content-item">
+		    	<td class="search-result"><a class="search-result__person-name" href="${data.url}">${data.name}</a></td>
+		  </tr>`;
+		  renderPersonDetails(".search-result a", searchResultsTable);
+      });
+    }
+  }
 }
 
 const renderPersonDetails = (keyMapElement, searchResultsContainer) => {
-	let keysMap = document.querySelectorAll(keyMapElement);
-	for (key in keysMap) {
-		keysMap[key].onclick = function(e) {
-			e.preventDefault();
-			let path = this.href;
-			let renderPlanetResidents = null;
-			fetch(path)
-				.then(data => data.json())
-				.then(data => {
-					let {name, birth_year, gender, homeworld} = data;
-					fetchPlanetResidents(homeworld);
-					setTimeout(() => {
-						searchResultsContainer.innerHTML = 
-							`<tr class="search-result___content-item">
-								<td class="search-result">
-									<p>Name: ${name}</p>
-									<p>Birth Year: ${birth_year}</p>
-									<p>Gender: ${gender}</p>
-									<p>Planet: ${planet}</p>
-									<p>${planet} Residents: </p>
-									<table id="renderResidentsTable" class="search-result__residents-table">
-									</table>
-								</td>
-							</tr>`;
-							let kek = document.querySelector('#renderResidentsTable');
-							renderResidents(kek, planetResidents);
-					}, 2000);		
-				});
+  let keysMap = document.querySelectorAll(keyMapElement);
+  for (key in keysMap) {
+	keysMap[key].onclick = function(e) {
+	  e.preventDefault();
+	  let path = this.href;
+	  let renderPlanetResidents = null;
+		fetch(path)
+	      .then(data => data.json())
+		  .then(data => {
+		    let {name, birth_year, gender, homeworld} = data;
+			fetchPlanetResidents(homeworld);
+			setTimeout(() => {
+			  searchResultsContainer.innerHTML = 
+				`<tr class="search-result___content-item">
+					<td class="search-result">
+						<p>Name: ${name}</p>
+						<p>Birth Year: ${birth_year}</p>
+						<p>Gender: ${gender}</p>
+						<p>Planet: ${planet}</p>
+						<p>${planet} Residents: </p>
+						<table id="renderResidentsTable" class="search-result__residents-table">
+						</table>
+					</td>
+				</tr>`;
+				let renderedResidents = document.querySelector('#renderResidentsTable');
+				renderResidents(renderedResidents, planetResidents);
+				}, 2000);		
+			});
 		}
 	}
 }
-
-
 
 searchButton.addEventListener('click', (e) => {
   if(searchInput.value){
